f6080f3c92b9347c993859eb28f781b32d5e4b4d
DanCristianIlie <ilie.dancrist@protonmail.com>
2022-09-12 23:49:00 +0300

    Brand setting

--- components/common/navbar/Navbar.js
+++ components/common/navbar/Navbar.js
@@ -10,66 +10,68 @@ function Navbar() {
 
 	return (
 		<>
-			<nav className='flex items-center flex-wrap bg-white p-3 '>
-				<Link href='/'>
-					<a className='inline-flex items-center p-2 mr-4 '>
+			<div className='fixed top-0 left-0 w-full bg-white'>
+				<nav className='flex items-center flex-wrap  p-3 max-w-7xl mx-auto'>
+					<Link href='/'>
+						<a className='inline-flex items-center p-2 mr-4 '>
+							<svg
+								viewBox='0 0 24 24'
+								xmlns='http://www.w3.org/2000/svg'
+								className='fill-current text-gold h-8 w-8 mr-2'>
+								<path d='M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z' />
+							</svg>
+							<span className='text-xl text-blue font-bold uppercase tracking-wide'>
+								mpp
+							</span>
+						</a>
+					</Link>
+					<button
+						className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-blue ml-auto hover:text-blue outline-none'
+						onClick={handleClick}>
 						<svg
+							className='w-6 h-6'
+							fill='none'
+							stroke='currentColor'
 							viewBox='0 0 24 24'
-							xmlns='http://www.w3.org/2000/svg'
-							className='fill-current text-white h-8 w-8 mr-2'>
-							<path d='M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z' />
+							xmlns='http://www.w3.org/2000/svg'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth={2}
+								d='M4 6h16M4 12h16M4 18h16'
+							/>
 						</svg>
-						<span className='text-xl text-gray font-bold uppercase tracking-wide'>
-							mpp
-						</span>
-					</a>
-				</Link>
-				<button
-					className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-gray ml-auto hover:text-blue outline-none'
-					onClick={handleClick}>
-					<svg
-						className='w-6 h-6'
-						fill='none'
-						stroke='currentColor'
-						viewBox='0 0 24 24'
-						xmlns='http://www.w3.org/2000/svg'>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							strokeWidth={2}
-							d='M4 6h16M4 12h16M4 18h16'
-						/>
-					</svg>
-				</button>
-				{/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
-				<div
-					className={`${
-						active ? "" : "hidden"
-					}   w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
-					<div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
-						<Link href='/'>
-							<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray font-bold items-center justify-center hover:bg-green-600 hover:text-white '>
-								Acasa
-							</a>
-						</Link>
-						<Link href='/'>
-							<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
-								Despre noi
-							</a>
-						</Link>
-						<Link href='/'>
-							<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
-								Domenii de practica
-							</a>
-						</Link>
-						<Link href='/'>
-							<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
-								Contact
-							</a>
-						</Link>
+					</button>
+					{/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+					<div
+						className={`${
+							active ? "" : "hidden"
+						}   w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
+						<div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+							<Link href='/'>
+								<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-blue font-bold items-center justify-center hover:bg-green-600 hover:text-gold '>
+									Acasa
+								</a>
+							</Link>
+							<Link href='/'>
+								<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-blue font-bold items-center justify-center hover:bg-green-600 hover:text-gold'>
+									Despre noi
+								</a>
+							</Link>
+							<Link href='/'>
+								<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-blue font-bold items-center justify-center hover:bg-green-600 hover:text-gold'>
+									Domenii de practica
+								</a>
+							</Link>
+							<Link href='/'>
+								<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-blue font-bold items-center justify-center hover:bg-green-600 hover:text-gold'>
+									Contact
+								</a>
+							</Link>
+						</div>
 					</div>
-				</div>
-			</nav>
+				</nav>
+			</div>
 		</>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,204 @@ export default function Home() {
 				<meta name='description' content='MARE BAROU' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultrices
+				sagittis ipsum in fringilla. Sed eu est sed ex ullamcorper consectetur.
+				Etiam consequat, justo vitae sagittis interdum, enim enim tincidunt
+				ligula, convallis sagittis diam nunc et nunc. Proin ac tristique risus,
+				at iaculis erat. Pellentesque libero est, pharetra ut laoreet in,
+				pretium a sapien. Phasellus mattis nulla ac libero porta sollicitudin.
+				Mauris et dui eget eros aliquam placerat et vel eros. Nunc a mauris
+				elementum, dictum elit rhoncus, placerat ante. In facilisis, mauris vel
+				elementum lacinia, urna nibh gravida purus, sed accumsan erat lorem a
+				dui. Nam a tortor id leo fringilla feugiat. Vestibulum ullamcorper
+				convallis tortor. Nulla eget ornare urna. Nulla quis elit suscipit,
+				molestie quam sit amet, egestas turpis. Nulla interdum mauris sed tempus
+				semper. Nulla dignissim sollicitudin sem, eu luctus lacus dignissim
+				vehicula. Quisque a malesuada nisl. Pellentesque luctus tincidunt velit,
+				vel sodales sapien scelerisque at. Nam dapibus lectus at velit
+				pellentesque tincidunt. Nunc eu justo sem. Nam sit amet rhoncus nisl. Ut
+				eget nibh sem. Proin vestibulum placerat pharetra. In venenatis ligula
+				in fermentum tristique. Suspendisse non sem feugiat, facilisis ante a,
+				dictum augue. Donec a euismod velit. Nulla dictum aliquet libero, sed
+				cursus diam accumsan non. Proin in lectus ut lacus feugiat convallis.
+				Sed sodales nisi dui, ut hendrerit turpis molestie sit amet. Donec
+				condimentum urna sed est aliquam pretium quis et diam. Suspendisse sed
+				tempor leo, et fringilla enim. Phasellus iaculis id tortor ac volutpat.
+				Etiam ullamcorper enim in laoreet venenatis. Fusce sagittis vulputate
+				euismod. Morbi et tortor fermentum, condimentum tellus nec, cursus orci.
+				Quisque euismod, ligula vitae lobortis bibendum, mauris velit interdum
+				ipsum, ac commodo turpis arcu vel nulla. Maecenas commodo nulla velit,
+				vel aliquam est blandit vitae. Nullam auctor nisi purus, at tempor quam
+				iaculis nec. Suspendisse congue mauris dictum massa tincidunt, eu
+				gravida turpis laoreet. Vestibulum ac ex lacus. Vestibulum ante ipsum
+				primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam
+				et interdum magna. Mauris erat sem, hendrerit vitae ex eu, eleifend
+				gravida ex. In sapien nunc, maximus quis elementum sed, luctus ac velit.
+				Phasellus rutrum sit amet felis nec placerat. Cras nisi leo, tincidunt
+				nec tellus ut, condimentum facilisis felis. Cras auctor convallis
+				tempor. Praesent eleifend posuere sollicitudin. Pellentesque porttitor
+				accumsan mi, at fermentum odio aliquet vel. Sed facilisis luctus
+				posuere. Nunc finibus blandit dolor non sodales. Donec iaculis euismod
+				urna vel cursus. Aliquam iaculis eu eros a viverra. Mauris augue libero,
+				aliquam et hendrerit et, consequat non ante. Fusce laoreet justo dui,
+				non iaculis enim malesuada vel. Nullam luctus imperdiet velit, nec
+				blandit nisi feugiat ac. Vivamus eget justo vulputate, dignissim lorem
+				a, suscipit sem. Aenean tempor viverra ipsum et rhoncus. Sed dignissim
+				pretium erat quis molestie. Vestibulum commodo faucibus venenatis. Sed
+				eu accumsan velit. Vivamus ut sem ligula. In rhoncus pharetra feugiat.
+				Phasellus egestas augue non sagittis blandit. Vivamus tincidunt sagittis
+				libero. Ut maximus diam et odio viverra vehicula.
+			</p>
+			<h1>bla nla</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultrices
+				sagittis ipsum in fringilla. Sed eu est sed ex ullamcorper consectetur.
+				Etiam consequat, justo vitae sagittis interdum, enim enim tincidunt
+				ligula, convallis sagittis diam nunc et nunc. Proin ac tristique risus,
+				at iaculis erat. Pellentesque libero est, pharetra ut laoreet in,
+				pretium a sapien. Phasellus mattis nulla ac libero porta sollicitudin.
+				Mauris et dui eget eros aliquam placerat et vel eros. Nunc a mauris
+				elementum, dictum elit rhoncus, placerat ante. In facilisis, mauris vel
+				elementum lacinia, urna nibh gravida purus, sed accumsan erat lorem a
+				dui. Nam a tortor id leo fringilla feugiat. Vestibulum ullamcorper
+				convallis tortor. Nulla eget ornare urna. Nulla quis elit suscipit,
+				molestie quam sit amet, egestas turpis. Nulla interdum mauris sed tempus
+				semper. Nulla dignissim sollicitudin sem, eu luctus lacus dignissim
+				vehicula. Quisque a malesuada nisl. Pellentesque luctus tincidunt velit,
+				vel sodales sapien scelerisque at. Nam dapibus lectus at velit
+				pellentesque tincidunt. Nunc eu justo sem. Nam sit amet rhoncus nisl. Ut
+				eget nibh sem. Proin vestibulum placerat pharetra. In venenatis ligula
+				in fermentum tristique. Suspendisse non sem feugiat, facilisis ante a,
+				dictum augue. Donec a euismod velit. Nulla dictum aliquet libero, sed
+				cursus diam accumsan non. Proin in lectus ut lacus feugiat convallis.
+				Sed sodales nisi dui, ut hendrerit turpis molestie sit amet. Donec
+				condimentum urna sed est aliquam pretium quis et diam. Suspendisse sed
+				tempor leo, et fringilla enim. Phasellus iaculis id tortor ac volutpat.
+				Etiam ullamcorper enim in laoreet venenatis. Fusce sagittis vulputate
+				euismod. Morbi et tortor fermentum, condimentum tellus nec, cursus orci.
+				Quisque euismod, ligula vitae lobortis bibendum, mauris velit interdum
+				ipsum, ac commodo turpis arcu vel nulla. Maecenas commodo nulla velit,
+				vel aliquam est blandit vitae. Nullam auctor nisi purus, at tempor quam
+				iaculis nec. Suspendisse congue mauris dictum massa tincidunt, eu
+				gravida turpis laoreet. Vestibulum ac ex lacus. Vestibulum ante ipsum
+				primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam
+				et interdum magna. Mauris erat sem, hendrerit vitae ex eu, eleifend
+				gravida ex. In sapien nunc, maximus quis elementum sed, luctus ac velit.
+				Phasellus rutrum sit amet felis nec placerat. Cras nisi leo, tincidunt
+				nec tellus ut, condimentum facilisis felis. Cras auctor convallis
+				tempor. Praesent eleifend posuere sollicitudin. Pellentesque porttitor
+				accumsan mi, at fermentum odio aliquet vel. Sed facilisis luctus
+				posuere. Nunc finibus blandit dolor non sodales. Donec iaculis euismod
+				urna vel cursus. Aliquam iaculis eu eros a viverra. Mauris augue libero,
+				aliquam et hendrerit et, consequat non ante. Fusce laoreet justo dui,
+				non iaculis enim malesuada vel. Nullam luctus imperdiet velit, nec
+				blandit nisi feugiat ac. Vivamus eget justo vulputate, dignissim lorem
+				a, suscipit sem. Aenean tempor viverra ipsum et rhoncus. Sed dignissim
+				pretium erat quis molestie. Vestibulum commodo faucibus venenatis. Sed
+				eu accumsan velit. Vivamus ut sem ligula. In rhoncus pharetra feugiat.
+				Phasellus egestas augue non sagittis blandit. Vivamus tincidunt sagittis
+				libero. Ut maximus diam et odio viverra vehicula.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultrices
+				sagittis ipsum in fringilla. Sed eu est sed ex ullamcorper consectetur.
+				Etiam consequat, justo vitae sagittis interdum, enim enim tincidunt
+				ligula, convallis sagittis diam nunc et nunc. Proin ac tristique risus,
+				at iaculis erat. Pellentesque libero est, pharetra ut laoreet in,
+				pretium a sapien. Phasellus mattis nulla ac libero porta sollicitudin.
+				Mauris et dui eget eros aliquam placerat et vel eros. Nunc a mauris
+				elementum, dictum elit rhoncus, placerat ante. In facilisis, mauris vel
+				elementum lacinia, urna nibh gravida purus, sed accumsan erat lorem a
+				dui. Nam a tortor id leo fringilla feugiat. Vestibulum ullamcorper
+				convallis tortor. Nulla eget ornare urna. Nulla quis elit suscipit,
+				molestie quam sit amet, egestas turpis. Nulla interdum mauris sed tempus
+				semper. Nulla dignissim sollicitudin sem, eu luctus lacus dignissim
+				vehicula. Quisque a malesuada nisl. Pellentesque luctus tincidunt velit,
+				vel sodales sapien scelerisque at. Nam dapibus lectus at velit
+				pellentesque tincidunt. Nunc eu justo sem. Nam sit amet rhoncus nisl. Ut
+				eget nibh sem. Proin vestibulum placerat pharetra. In venenatis ligula
+				in fermentum tristique. Suspendisse non sem feugiat, facilisis ante a,
+				dictum augue. Donec a euismod velit. Nulla dictum aliquet libero, sed
+				cursus diam accumsan non. Proin in lectus ut lacus feugiat convallis.
+				Sed sodales nisi dui, ut hendrerit turpis molestie sit amet. Donec
+				condimentum urna sed est aliquam pretium quis et diam. Suspendisse sed
+				tempor leo, et fringilla enim. Phasellus iaculis id tortor ac volutpat.
+				Etiam ullamcorper enim in laoreet venenatis. Fusce sagittis vulputate
+				euismod. Morbi et tortor fermentum, condimentum tellus nec, cursus orci.
+				Quisque euismod, ligula vitae lobortis bibendum, mauris velit interdum
+				ipsum, ac commodo turpis arcu vel nulla. Maecenas commodo nulla velit,
+				vel aliquam est blandit vitae. Nullam auctor nisi purus, at tempor quam
+				iaculis nec. Suspendisse congue mauris dictum massa tincidunt, eu
+				gravida turpis laoreet. Vestibulum ac ex lacus. Vestibulum ante ipsum
+				primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam
+				et interdum magna. Mauris erat sem, hendrerit vitae ex eu, eleifend
+				gravida ex. In sapien nunc, maximus quis elementum sed, luctus ac velit.
+				Phasellus rutrum sit amet felis nec placerat. Cras nisi leo, tincidunt
+				nec tellus ut, condimentum facilisis felis. Cras auctor convallis
+				tempor. Praesent eleifend posuere sollicitudin. Pellentesque porttitor
+				accumsan mi, at fermentum odio aliquet vel. Sed facilisis luctus
+				posuere. Nunc finibus blandit dolor non sodales. Donec iaculis euismod
+				urna vel cursus. Aliquam iaculis eu eros a viverra. Mauris augue libero,
+				aliquam et hendrerit et, consequat non ante. Fusce laoreet justo dui,
+				non iaculis enim malesuada vel. Nullam luctus imperdiet velit, nec
+				blandit nisi feugiat ac. Vivamus eget justo vulputate, dignissim lorem
+				a, suscipit sem. Aenean tempor viverra ipsum et rhoncus. Sed dignissim
+				pretium erat quis molestie. Vestibulum commodo faucibus venenatis. Sed
+				eu accumsan velit. Vivamus ut sem ligula. In rhoncus pharetra feugiat.
+				Phasellus egestas augue non sagittis blandit. Vivamus tincidunt sagittis
+				libero. Ut maximus diam et odio viverra vehicula.
+			</p>
+			<h1>bla nla</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultrices
+				sagittis ipsum in fringilla. Sed eu est sed ex ullamcorper consectetur.
+				Etiam consequat, justo vitae sagittis interdum, enim enim tincidunt
+				ligula, convallis sagittis diam nunc et nunc. Proin ac tristique risus,
+				at iaculis erat. Pellentesque libero est, pharetra ut laoreet in,
+				pretium a sapien. Phasellus mattis nulla ac libero porta sollicitudin.
+				Mauris et dui eget eros aliquam placerat et vel eros. Nunc a mauris
+				elementum, dictum elit rhoncus, placerat ante. In facilisis, mauris vel
+				elementum lacinia, urna nibh gravida purus, sed accumsan erat lorem a
+				dui. Nam a tortor id leo fringilla feugiat. Vestibulum ullamcorper
+				convallis tortor. Nulla eget ornare urna. Nulla quis elit suscipit,
+				molestie quam sit amet, egestas turpis. Nulla interdum mauris sed tempus
+				semper. Nulla dignissim sollicitudin sem, eu luctus lacus dignissim
+				vehicula. Quisque a malesuada nisl. Pellentesque luctus tincidunt velit,
+				vel sodales sapien scelerisque at. Nam dapibus lectus at velit
+				pellentesque tincidunt. Nunc eu justo sem. Nam sit amet rhoncus nisl. Ut
+				eget nibh sem. Proin vestibulum placerat pharetra. In venenatis ligula
+				in fermentum tristique. Suspendisse non sem feugiat, facilisis ante a,
+				dictum augue. Donec a euismod velit. Nulla dictum aliquet libero, sed
+				cursus diam accumsan non. Proin in lectus ut lacus feugiat convallis.
+				Sed sodales nisi dui, ut hendrerit turpis molestie sit amet. Donec
+				condimentum urna sed est aliquam pretium quis et diam. Suspendisse sed
+				tempor leo, et fringilla enim. Phasellus iaculis id tortor ac volutpat.
+				Etiam ullamcorper enim in laoreet venenatis. Fusce sagittis vulputate
+				euismod. Morbi et tortor fermentum, condimentum tellus nec, cursus orci.
+				Quisque euismod, ligula vitae lobortis bibendum, mauris velit interdum
+				ipsum, ac commodo turpis arcu vel nulla. Maecenas commodo nulla velit,
+				vel aliquam est blandit vitae. Nullam auctor nisi purus, at tempor quam
+				iaculis nec. Suspendisse congue mauris dictum massa tincidunt, eu
+				gravida turpis laoreet. Vestibulum ac ex lacus. Vestibulum ante ipsum
+				primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam
+				et interdum magna. Mauris erat sem, hendrerit vitae ex eu, eleifend
+				gravida ex. In sapien nunc, maximus quis elementum sed, luctus ac velit.
+				Phasellus rutrum sit amet felis nec placerat. Cras nisi leo, tincidunt
+				nec tellus ut, condimentum facilisis felis. Cras auctor convallis
+				tempor. Praesent eleifend posuere sollicitudin. Pellentesque porttitor
+				accumsan mi, at fermentum odio aliquet vel. Sed facilisis luctus
+				posuere. Nunc finibus blandit dolor non sodales. Donec iaculis euismod
+				urna vel cursus. Aliquam iaculis eu eros a viverra. Mauris augue libero,
+				aliquam et hendrerit et, consequat non ante. Fusce laoreet justo dui,
+				non iaculis enim malesuada vel. Nullam luctus imperdiet velit, nec
+				blandit nisi feugiat ac. Vivamus eget justo vulputate, dignissim lorem
+				a, suscipit sem. Aenean tempor viverra ipsum et rhoncus. Sed dignissim
+				pretium erat quis molestie. Vestibulum commodo faucibus venenatis. Sed
+				eu accumsan velit. Vivamus ut sem ligula. In rhoncus pharetra feugiat.
+				Phasellus egestas augue non sagittis blandit. Vivamus tincidunt sagittis
+				libero. Ut maximus diam et odio viverra vehicula.
+			</p>
 		</>
 	);
 }
