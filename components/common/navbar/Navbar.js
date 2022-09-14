fead14fa4e79c80f94d49e56ff9677ca03236fcb
DanCristianIlie <ilie.dancrist@protonmail.com>
2022-09-14 20:55:44 +0300

    Navbar logo

--- components/common/navbar/Navbar.js
+++ components/common/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useState } from "react";
+import Image from "next/image";
 
 function Navbar() {
 	const [active, setActive] = useState(false);
@@ -10,19 +11,11 @@ function Navbar() {
 
 	return (
 		<>
-			<nav className='bg-ivory px-2 sm:px-4 py-2.5  fixed w-full z-20 top-0 left-0 border-b border-gold'>
+			<nav className='bg-ivory px-2 sm:px-4 py-2.5  fixed w-screen z-20 top-0 left-0 border-b border-gold'>
 				<div className='container flex flex-wrap justify-between items-center mx-auto max-w-7xl'>
 					<Link href='/'>
 						<a className='flex items-center'>
-							<svg
-								viewBox='0 0 24 24'
-								xmlns='http://www.w3.org/2000/svg'
-								className='mr-3 h-6 sm:h-9'>
-								<path d='M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z' />
-							</svg>
-							<span className='self-center text-xl font-bold whitespace-nowrap uppercase'>
-								Mpp
-							</span>
+							<Image src='/logo.svg' height={50} width={100} />
 						</a>
 					</Link>
 
