5527f88aeb5283528dcbedb2a789891279785faa
DanCristianIlie <ilie.dancrist@protonmail.com>
2022-09-25 19:00:06 +0300

    Github error

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import Layout from "../components/common/Layout";
+import Layout from "../components/common/layout";
 
 function MyApp({ Component, pageProps }) {
 	return (
